dc02a93b6eb582a039e8cf5acf026fba6d0ca725
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-19 11:13:42 -0400

    Add files via upload

---
+++ Intro_to_blocks_practice.rb
@@ -0,0 +1,8 @@
+# Define a print_five_times method that accepts no arguments.
+# It should output the text "Hello" 5 times using the times method.
+# Use the print method in your solution.
+
+def print_five_times
+  5. times {print "Hello"}
+end
+
