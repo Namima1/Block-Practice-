8a292c64908607d01a7a9e1a590e9ed972b78687
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-19 11:15:04 -0400

    Add files via upload

---
+++ upto_downto_method.rb
@@ -0,0 +1,9 @@
+2.upto(10) { |current| puts "The loop is  now on: #{current}" }
+#upto method lets us go up to a certain number 
+#the 2 indicates where to begin
+#the 10 indicated which number to stop at
+#curly braces is the block
+
+5.downto(1) { |number| puts "Countdown: #{number}" }
+#5 is the integer it starts
+#1 is the integer it gets to 
