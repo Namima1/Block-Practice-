59257a96700f63e592307d70c2d536cd038031a4
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-07-19 11:12:44 -0400

    Add files via upload

---
+++ Block_practice.rb
@@ -0,0 +1,13 @@
+#Argument is an object with a concrete value. 
+#Blocks are a collection of code with a procedure. 
+
+5. times {puts "Coding in Ruby."}
+#this integer and method(5.times) is asking to run something 5 times 
+#curly braces is the block 
+
+3.times do
+  puts "Coding"
+  puts "In Ruby" 
+end 
+#Use above syntax if you have more than one line of code 
+#"do" begins block, "end" temrinates block and what is between do and end is what ruby will run
